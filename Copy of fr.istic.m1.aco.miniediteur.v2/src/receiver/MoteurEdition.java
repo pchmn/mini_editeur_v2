0734061ad0118aff6bff8f258e7987429a9dfa72
pchmn <couleurwhatever@148.60.14.93>
2015-12-03 13:33:39 +0100

    vraie final

--- Copy of fr.istic.m1.aco.miniediteur.v2/src/receiver/MoteurEdition.java
+++ Copy of fr.istic.m1.aco.miniediteur.v2/src/receiver/MoteurEdition.java
@@ -132,7 +132,7 @@ public class MoteurEdition implements MoteurEditionInterface
 			this.selection.setLongueur(longueur);
 			this.selection.setContenu(this.buffer.getContenu().substring(debut, debut + longueur));
 		} catch (Exception e) {
-			System.err.println("\nVotre sélection dépasse la longueur du texte.\nLa sélection est mise à la fin du texte. \n");
+			System.out.println("\nVotre sélection dépasse la longueur du texte.\nLa sélection est mise à la fin du texte.");
 			this.selection.setDebut(this.buffer.getContenu().toString().length());
 			this.selection.setLongueur(0);
 			this.selection.setContenu("");
